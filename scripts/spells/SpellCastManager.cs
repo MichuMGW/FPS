b8ca9ed876c2ca34c3f4e4177f6f678ed8198779
MichuMGW <iver3n@gmail.com>
2025-03-12 11:42:26 +0100

    Modyfikacja klasy SpellCastManager

--- scripts/spells/SpellCastManager.cs
+++ scripts/spells/SpellCastManager.cs
@@ -5,19 +5,45 @@ using System.Collections.Generic;
 public partial class SpellCastManager : Node
 {
 	public List<Spell> spells = new List<Spell>();
-
+    public int currentSpell = 0;
+    
     public override void _Ready()
     {
         spells.Add(new ProjectileSpell());
     }
 
+    public override void _Input(InputEvent @event)
+    {
+        
+    }
+
     public override void _Process(double delta)
     {
+        //INPUTS
+
         if (Input.IsActionJustPressed("CastSpell")){
 			CharacterBody3D player = GetOwner<CharacterBody3D>();
 			Node3D playerCamera = player.GetNode<Camera3D>("Head/Camera3D");
-			var spell = spells[0];
+			var spell = spells[currentSpell];
 			spell.Cast(player.GlobalTransform.Origin, -playerCamera.GlobalTransform.Basis.Z, player);
 		}
+
+        if(Input.IsActionJustPressed("NextSpell")){
+            if(spells.Count == currentSpell){
+                currentSpell = 0;
+            }
+            else {
+                currentSpell++;
+            }
+        }
+        if(Input.IsActionJustPressed("PreviousSpell")){
+            if(currentSpell == 0){
+                currentSpell = spells.Count - 1;
+            }
+            else {
+                currentSpell--;
+            }
+        }
+        //INPUTS
     }
 }
