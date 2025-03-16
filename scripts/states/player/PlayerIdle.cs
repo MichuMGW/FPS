4c9e6ce5664302e1d9ed96a8bdbab30a4b94ca8c
MichuMGW <iver3n@gmail.com>
2025-03-16 16:11:01 +0100

    Player states added (incomplete)

---
+++ scripts/states/player/PlayerIdle.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public partial class PlayerIdle : State
+{
+	SpellCastManager spm;
+    public override void Enter()
+    {
+        //TODO: Animacja domyślna
+		spm = GetParent() as SpellCastManager;
+    }
+
+    public override void Exit()
+    {
+        return;
+    }
+
+    public override void Update(double delta)
+    {
+        if(Input.IsActionJustPressed("NextSpell")){
+            if(spm.spells.Count - 1 == spm.currentSpell){
+                spm.currentSpell = 0;
+            }
+            else {
+                spm.currentSpell++;
+            }
+        }
+        if(Input.IsActionJustPressed("PreviousSpell")){
+            if(spm.currentSpell == 0){
+                spm.currentSpell = spm.spells.Count - 1;
+            }
+            else {
+                spm.currentSpell--;
+            }
+        }
+		if (Input.IsActionJustPressed("CastSpell")){
+			EmitSignal(SignalName.Transitioned, this, "PlayerCast");
+		}
+    }
+
+    public override void Physics_Update(double delta)
+    {
+        return;
+    }
+
+}
---
+++ scripts/states/player/PlayerStateMachine.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public partial class PlayerStateMachine : StateMachine
+{
+	public SpellCastManager Spm {get; set;}
+}
