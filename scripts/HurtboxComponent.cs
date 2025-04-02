52b990463135b0206c8c5c93687be5d54e5f9215
MichuMGW <iver3n@gmail.com>
2025-04-03 00:53:15 +0200

    Added status component to enemy Removed unused ElementPair class

--- scripts/HurtboxComponent.cs
+++ scripts/HurtboxComponent.cs
@@ -3,8 +3,8 @@ using System;
 
 public partial class HurtboxComponent : Area3D
 {
-	[Export]
-	public HealthComponent healthComponent;
+	
+	[Export] public HealthComponent healthComponent;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
--- scripts/components/StatusComponent.cs
+++ scripts/components/StatusComponent.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using Godot.Collections;
 
 public partial class StatusComponent : Node
 {
@@ -9,38 +10,96 @@ public partial class StatusComponent : Node
 	[Signal] public delegate void OnSlowedEventHandler();
 	[Signal] public delegate void OnStunnedEventHandler();
 	[Signal] public delegate void OnFrozenEventHandler();
+	[Signal] public delegate void OnFireTimeoutEventHandler();
+	[Signal] public delegate void OnSlowedTimeoutEventHandler();
+	[Signal] public delegate void OnStunnedTimeoutEventHandler();
+	[Signal] public delegate void OnFrozenTimeoutEventHandler();
 	//TODO: Jakbym chciał dodać animacje przy spaleniu
 	//[Signal] public delegate void OnBurnedEventHandler();
-	public bool isOnFire = false;
-	public bool isSlowed = false;
-	public bool isStunned = false;
-	public bool isFrozen = false;
+	public bool IsOnFire {get; set;} = false;
+	public bool IsSlowed {get; set;} = false;
+	public bool IsStunned {get; set;} = false;
+	public bool IsFrozen {get; set;} = false;
+	public Timer FireTimer {get; set;}
+	public Timer SlowTimer {get; set;}
+	public Timer StunTimer {get; set;}
+	public Timer FreezeTimer {get; set;}
+
+	// private Dictionary statusTimers;
 	public override void _Ready()
 	{
+		FireTimer = GetNodeOrNull<Timer>("FireTimer");
+		SlowTimer = GetNodeOrNull<Timer>("SlowTimer");
+		StunTimer = GetNodeOrNull<Timer>("StunTimer");
+		FreezeTimer = GetNodeOrNull<Timer>("FreezeTimer");
+		// statusTimers = new Dictionary();
+		// foreach (Timer child in GetChildren()){
+		// 	if (child is Timer){
+		// 		statusTimers[child.Name] = child;
+		// 		child.Timeout += () => {
+		// 			if (child.Name == "OnFireTimer"){
+		// 				IsOnFire = false;
+		// 				EmitSignal(nameof(OnFire));
+		// 				GD.Print("Off Fire");
+		// 			}
+		// 			if (child.Name == "OnFrozenTimer"){
+		// 				IsFrozen = false;
+		// 				GD.Print("Unfrozen");
+		// 			}
+		// 			if (child.Name == "OnStunnedTimer"){
+		// 				IsStunned = false;
+		// 				GD.Print("Unstunned");
+		// 			}
+		// 			if (child.Name == "OnSlowedTimer"){
+		// 				IsSlowed = false;
+		// 				GD.Print("Unslowed");
+		// 			}
+		// 		};
 
+		// 	}
+		// }
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	private void InitializeTimers(){
+		FireTimer = GetNodeOrNull<Timer>("FireTimer");
+		SlowTimer = GetNodeOrNull<Timer>("SlowTimer");
+		StunTimer = GetNodeOrNull<Timer>("StunTimer");
+		FreezeTimer = GetNodeOrNull<Timer>("FreezeTimer");
+
+		FireTimer.Timeout += () => {
+			IsOnFire = false;
+			EmitSignal(nameof(OnFireTimeout));
+		};
+
+		SlowTimer.Timeout += () => {
+			IsSlowed = false;
+			EmitSignal(nameof(OnSlowedTimeout));
+		};
+
+		StunTimer.Timeout += () => {
+			IsStunned = false;
+			EmitSignal(nameof(OnStunnedTimeout));
+		};
+
+		FreezeTimer.Timeout += () => {
+			IsFrozen = false;
+			EmitSignal(nameof(OnFrozenTimeout));
+		};
 	}
 
 	public void ApplyStatusEffect(Element firstElement, Element secondElement){
 		if (firstElement == Element.Fire || secondElement == Element.Fire){
-			isOnFire = true;
+			IsOnFire = true;
 			EmitSignal(nameof(OnFire));
 			GD.Print("On Fire");
 		}
 		if (firstElement == Element.Water || secondElement == Element.Water){
-			isFrozen = true;
-			GD.Print("Frozen");
-		}
-		if (firstElement == Element.Earth || secondElement == Element.Earth){
-			isSlowed = true;
+			IsSlowed = true;
 			GD.Print("Slowed");
 		}
-		if (firstElement == Element.Air || secondElement == Element.Air){
-			isStunned = true;
+		if (firstElement == Element.Earth || secondElement == Element.Earth){
+			//dodać obsługę stunów
+			IsStunned = true;
 			GD.Print("Stunned");
 		}
 	}
