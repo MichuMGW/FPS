841efb280626595f6d76006c6582c41c9a39b0fc
MichuMGW <iver3n@gmail.com>
2025-11-13 01:17:47 +0100

    do poprawy

---
+++ scripts/entity/enemy/TrollArcher.cs
@@ -0,0 +1,23 @@
+// using Godot;
+// using System;
+
+// public partial class TrollArcher : CharacterBody3D
+// {
+//     [Export] public HealthComponent Health;
+//     [Export] public MovementComponent Movement;
+//     [Export] public StatusComponent Status;
+//     [Export] public ExperienceDropComponent ExperienceDrop;
+//     [Export] public NavigationComponent Navigation;
+//     [Export] public HitboxComponent Hitbox;
+
+//     public override void _Ready()
+//     {
+//         Health = GetNode<HealthComponent>("HealthComponent");
+//         Movement = GetNode<MovementComponent>("MovementComponent");
+//         Status = GetNode<StatusComponent>("StatusComponent");
+//         ExperienceDrop = GetNode<ExperienceDropComponent>("ExperienceDropComponent");
+//         Navigation = GetNode<NavigationComponent>("NavigationComponent");
+//         Hitbox = GetNode<HitboxComponent>("HitboxComponent");
+//     }
+
+// }
